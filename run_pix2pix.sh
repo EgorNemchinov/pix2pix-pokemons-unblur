38f66a3e4de9fbc598c8d68bf080e357f6403007
Egor Nemchinov <immortalsuperturtle@gmail.com>
2018-07-20 15:33:39 +0300

    Add scripts for preparing data and running network

---
+++ run_pix2pix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 pytorch_pix2pix.py --dataset pokemons
+                           --test_subfolder test
---
+++ split_files.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import os
+from os.path import join, isfile
+from shutil import move
+
+train_size = 0.75
+test_size = 0.2
+
+dir = './data/pokemons/'
+os.mkdir(join(dir, 'train'))
+os.mkdir(join(dir, 'test'))
+os.mkdir(join(dir, 'val'))
+
+imgs = [f for f in os.listdir(dir) if isfile(join(dir, f)) and
+                                      f.endswith('jpg')]
+
+train_size = int(len(imgs)*train_size)
+test_size = int(len(imgs)*test_size)
+
+for img in imgs[:train_size]:
+    move(join(dir, img), join(dir, 'train/', img))
+for img in imgs[train_size:train_size+test_size]:
+    move(join(dir, img), join(dir, 'test/', img))
+for img in imgs[train_size+test_size:]:
+    move(join(dir, img), join(dir, 'val/', img))
+
